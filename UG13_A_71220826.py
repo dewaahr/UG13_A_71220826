9fcbc5e0b70421d2a7e4edd8d4df20b4126108c8
darkoboy <118077486+dewaahr@users.noreply.github.com>
2022-12-16 10:24:12 +0700

    Add files via upload

---
+++ UG13_A_71220826.py
@@ -0,0 +1,70 @@
+import turtle
+s = turtle.Screen()
+t = turtle.Turtle()
+#nama : Hermanus Rangga Horo
+#nim : 712220826
+#H826R
+
+#H
+t.up()
+t.goto(-300,0)
+t.down()
+t.pencolor("#FFA500")
+t.fillcolor("#FFA500")
+t.begin_fill()
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(25)
+t.right(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.right(90)
+t.forward(25)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(25)
+t.right(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.right(90)
+t.forward(25)
+t.end_fill()
+
+#8
+t.pensize(15)
+t.pencolor("#FFA500")
+t.up()
+t.goto(-160,50)
+t.down()
+t.circle(30)
+t.up()
+t.goto(-160,100)
+t.down()
+t.circle(25)
+t.up()
+
+#2
+t.goto(-50,80)
+
+t.down()
+t.right(50)
+t.circle(45, 95)
+t.up()
+t.goto(-50,80)
+t.pen
+t.right(90)
+t.forward(50)
+
+
+
+
+
+s.exitonclick()
